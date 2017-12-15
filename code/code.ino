67140f522a99a122bf3f4d7d117e5848cd8cbc9b
Philippe Loctaux <loctauxphilippe@gmail.com>
2017-12-15 10:59:10 +0100

    ajout de code.ino
    c'est parti

---
+++ code/code.ino
@@ -0,0 +1,18 @@
+/*
+ * code.ino : code pour l'esp32
+ * 
+ * par Philippe Loctaux
+ * par Yann Le Guehennec
+ * 
+ * https://poubelle.online
+ * https://github.com/vegapoubelle
+ * 
+ */
+
+void setup() {
+
+}
+
+void loop() {
+
+}
