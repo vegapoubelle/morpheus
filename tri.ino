d70b851449fc912b66856fbc5091dd437d4bfe63
Yann Le Guehennec <ylgdu56@gmail.com>
2018-01-11 09:54:07 +0100

    ajout des trucs a faire

--- tri.ino
+++ tri.ino
@@ -7,3 +7,21 @@
  * https://github.com/vegapoubelle
  * 
  */
+
+
+ /* Liste de truc a trier :
+  *  * métaux
+  *  * poids
+  *  * rigidité
+  *  
+  *  
+  *  étapes de tri:
+  *  1. Métaux
+  *       oui: dans le bac métal
+  *  2. Rigidité
+  *       si oui et poids > valeur: dans le bac verre
+  *       si oui et poids < valeur: dans le bac plastique
+  *       non: 
+  *  
+  *  
+  */
