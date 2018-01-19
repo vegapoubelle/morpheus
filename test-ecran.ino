e063c0a6f984372af22cd9fff2ff1790a7cb2d4a
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-01-19 11:39:11 +0100

    afficher le site

--- test-ecran.ino
+++ test-ecran.ino
@@ -1,4 +1,34 @@
 /* note: il faut installer les bibliotheques
- * https://github.com/adafruit/Adafruit-GFX-Library
- * https://github.com/adafruit/Adafruit_SSD1306
- */
+   https://github.com/adafruit/Adafruit-GFX-Library
+   https://github.com/adafruit/Adafruit_SSD1306
+*/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+void setup()   {
+  Serial.begin(9600);
+
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+  // Clear the buffer.
+  display.clearDisplay();
+
+  display.setCursor(0, 0);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.println("poubelle.online");
+  display.display();
+}
+
+/* vim: set tabstop=2: */
