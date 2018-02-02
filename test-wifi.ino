62eb09e84958efd7abd5d1793863a3683a45b9ba
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-02-02 11:48:15 +0100

    works

--- test-wifi.ino
+++ test-wifi.ino
@@ -10,55 +10,8 @@
 
 #include <WiFi.h>
 
-const char* network = "ssid";
-const char* password = "pssw";
-const char* server = "poubelle.online";
-
-/* tls certificate:
- *
- * linux: install `openssl`
- * windows: check online `openssl client windows`
- *
- * command on arch: "openssl s_client -showcerts -connect poubelle.online:443 </dev/null "
- */
-
-/* let's encrypt rootCA */
-const char* rootCA = \
-"----BEGIN CERTIFICATE-----\n" \
-"IIEkjCCA3qgAwIBAgIQCgFBQgAAAVOFc2oLheynCDANBgkqhkiG9w0BAQsFADA/\n" \
-"SQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n" \
-"kRTVCBSb290IENBIFgzMB4XDTE2MDMxNzE2NDA0NloXDTIxMDMxNzE2NDA0Nlow\n" \
-"jELMAkGA1UEBhMCVVMxFjAUBgNVBAoTDUxldCdzIEVuY3J5cHQxIzAhBgNVBAMT\n" \
-"kxldCdzIEVuY3J5cHQgQXV0aG9yaXR5IFgzMIIBIjANBgkqhkiG9w0BAQEFAAOC\n" \
-"Q8AMIIBCgKCAQEAnNMM8FrlLke3cl03g7NoYzDq1zUmGSXhvb418XCSL7e4S0EF\n" \
-"6meNQhY7LEqxGiHC6PjdeTm86dicbp5gWAf15Gan/PQeGdxyGkOlZHP/uaZ6WA8\n" \
-"Mx+yk13EiSdRxta67nsHjcAHJyse6cF6s5K671B5TaYucv9bTyWaN8jKkKQDIZ0\n" \
-"8h/pZq4UmEUEz9l6YKHy9v6Dlb2honzhT+Xhq+w3Brvaw2VFn3EK6BlspkENnWA\n" \
-"6xK8xuQSXgvopZPKiAlKQTGdMDQMc2PMTiVFrqoM7hD8bEfwzB/onkxEz0tNvjj\n" \
-"PIzark5McWvxI0NHWQWM6r6hCm21AvA2H3DkwIDAQABo4IBfTCCAXkwEgYDVR0T\n" \
-"QH/BAgwBgEB/wIBADAOBgNVHQ8BAf8EBAMCAYYwfwYIKwYBBQUHAQEEczBxMDIG\n" \
-"CsGAQUFBzABhiZodHRwOi8vaXNyZy50cnVzdGlkLm9jc3AuaWRlbnRydXN0LmNv\n" \
-"TA7BggrBgEFBQcwAoYvaHR0cDovL2FwcHMuaWRlbnRydXN0LmNvbS9yb290cy9k\n" \
-"3Ryb290Y2F4My5wN2MwHwYDVR0jBBgwFoAUxKexpHsscfrb4UuQdf/EFWCFiRAw\n" \
-"AYDVR0gBE0wSzAIBgZngQwBAgEwPwYLKwYBBAGC3xMBAQEwMDAuBggrBgEFBQcC\n" \
-"RYiaHR0cDovL2Nwcy5yb290LXgxLmxldHNlbmNyeXB0Lm9yZzA8BgNVHR8ENTAz\n" \
-"DGgL6AthitodHRwOi8vY3JsLmlkZW50cnVzdC5jb20vRFNUUk9PVENBWDNDUkwu\n" \
-"3JsMB0GA1UdDgQWBBSoSmpjBH3duubRObemRWXv86jsoTANBgkqhkiG9w0BAQsF\n" \
-"AOCAQEA3TPXEfNjWDjdGBX7CVW+dla5cEilaUcne8IkCJLxWh9KEik3JHRRHGJo\n" \
-"M2VcGfl96S8TihRzZvoroed6ti6WqEBmtzw3Wodatg+VyOeph4EYpr/1wXKtx8/\n" \
-"ApIvJSwtmVi4MFU5aMqrSDE6ea73Mj2tcMyo5jMd6jmeWUHK8so/joWUoHOUgwu\n" \
-"4Po1QYz+3dszkDqMp4fklxBwXRsW10KXzPMTZ+sOPAveyxindmjkW8lGy+QsRlG\n" \
-"fZ+G6Z6h7mjem0Y+iWlkYcV4PIWL1iwBi8saCbGS5jN2p8M+X+Q7UNKEkROb3N6\n" \
-"Oqkqm57TH2H3eDJAkSnh6/DNFu0Qg==\n" \
-"----END CERTIFICATE-----\n";
-
-
-const char* cert =\
-"cert goes here\n" \
-"multiple lines are done like that\n";
-
-/* make an instance of WiFiClientSecure */
-WiFiClientSecure client;
+const char* network = "phil's phone";
+const char* password = "mechkeyb";
 
 void setup() {
   Serial.begin(9600);
@@ -73,40 +26,6 @@ void setup() {
   Serial.print("connexion etablie au reseau: ");
   Serial.println(network);
 
-  /* setup the tls certificate */
-  client.setCACert(cert);
-
-
-  Serial.println("connexion au server sur le port 443");
-  if (!client.connect(server, 443)) {
-    Serial.println(":(");
-  } else {
-    Serial.println(":)");
-    /* create HTTPS request */
-    client.println("GET https://poubelle.online/ HTTP/1.0");
-    client.println("Host: poubelle.online");
-    client.println("Connection: close");
-    client.println();
-
-    Serial.print("en attente d'une reponse");
-    while (!client.available()) {
-      delay(50);
-      Serial.print(".");
-    }
-
-    /* if data is available then receive and print to Terminal */
-    while (client.available()) {
-      char c = client.read();
-      Serial.write(c);
-    }
-
-    /* if the server disconnected, stop the client */
-    if (!client.connected()) {
-      Serial.println();
-      Serial.println("Server disconnected");
-      client.stop();
-    }
-  }
 }
 
 void loop() {
