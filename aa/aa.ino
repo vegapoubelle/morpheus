d745aa3be96def890b2cceb3bb50f248f000f9e1
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-02-15 08:54:50 +0100

    works

--- aa/aa.ino
+++ aa/aa.ino
@@ -10,83 +10,70 @@
 
 const char* ssid     = "phil's phone";
 const char* password = "mechkeyb";
-
 const char* host = "poubelle.online";
+String url = "/test.txt";
 
-void setup()
-{
-    Serial.begin(9600);
-    delay(10);
+void setup() {
+  Serial.begin(9600);
 
-    // We start by connecting to a WiFi network
+  /* connecting to wifi */
+  WiFi.begin(ssid, password);
 
-    Serial.println();
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
+  /* waiting for wifi to connect */
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 
-    WiFi.begin(ssid, password);
+  Serial.print("up: ");
+  Serial.print(ssid);
+  Serial.print(" @ ");
+  Serial.println(WiFi.localIP());
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
+
+  /* you can loop that shit here if you want */ 
+  /* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  /* create tcp connexion */
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  /* request url to host */
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+      "Host: " + host + "\r\n" +
+      "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
     }
+  }
 
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+  /* read reply and print to serial */
+  while(client.available()) {
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
 }
 
-int value = 0;
-
-void loop()
-{
-    delay(5000);
-    ++value;
-
-    Serial.print("connecting to ");
-    Serial.println(host);
-
-    // Use WiFiClient class to create TCP connections
-    WiFiClient client;
-    const int httpPort = 80;
-    if (!client.connect(host, httpPort)) {
-        Serial.println("connection failed");
-        return;
-    }
-
-    // We now create a URI for the request
-    String url = "/input/";
-    url += streamId;
-    url += "?private_key=";
-    url += privateKey;
-    url += "&value=";
-    url += value;
-
-    Serial.print("Requesting URL: ");
-    Serial.println(url);
-
-    // This will send the request to the server
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + host + "\r\n" +
-                 "Connection: close\r\n\r\n");
-    unsigned long timeout = millis();
-    while (client.available() == 0) {
-        if (millis() - timeout > 5000) {
-            Serial.println(">>> Client Timeout !");
-            client.stop();
-            return;
-        }
-    }
-
-    // Read all the lines of the reply from server and print them to Serial
-    while(client.available()) {
-        String line = client.readStringUntil('\r');
-        Serial.print(line);
-    }
-
-    Serial.println();
-    Serial.println("closing connection");
+void loop() {
+  /* empty loop */
 }
+
+/* vim: set ts=2 sw=2 et : */
 
