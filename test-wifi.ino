39888bfd3e8c5b00adc126acb1feff098b02e570
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-03-16 09:40:43 +0100

    new path for php

--- test-wifi.ino
+++ test-wifi.ino
@@ -53,7 +53,7 @@ void loop() {
     Serial.println(masse);
 
     /* send the http get request */
-    client.print("GET /add.php?");
+    client.print("GET /php/add.php?");
 
     /* send the value of 'masse' */
     client.print("masse=");
