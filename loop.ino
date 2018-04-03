ff24a687784757c55ac5a513c2b4dbb24ee81fa2
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-04-03 21:45:26 +0200

    added loop and variables

---
+++ loop.ino
@@ -0,0 +1,12 @@
+/*
+ * loop.ino : la fonction qui tourne à ∞
+ * 
+ * http://poubelle.online
+ * 
+ */
+
+void loop() {
+
+}
+
+/* vim: set ts=2 sw=2 et : */
---
+++ variables.ino
@@ -0,0 +1,8 @@
+/*
+ * variables.ino: declaration des variables
+ * 
+ * http://poubelle.online
+ * 
+ */
+
+/* vim: set ts=2 sw=2 et : */
