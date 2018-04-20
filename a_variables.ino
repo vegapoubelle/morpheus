2e653045206ad66825f84ec93bca70e6df8516f9
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-04-20 09:08:22 +0200

    removed 'const' for the wifi variables
    maybe that was the issue

--- a_variables.ino
+++ a_variables.ino
@@ -6,8 +6,8 @@
 */
 
 /* wifi */
-const char* wifi_network     = "deadbaed";
-const char* wifi_password = "mechkeybb";
+char* wifi_network     = "deadbaed";
+char* wifi_password = "mechkeybb";
 const char* host = "poubelle.online";
 const int port = 80;
 int masse, matiere = 4;
