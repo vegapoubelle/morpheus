109c98aa16f977b6b41edb238d482ccfcf0e5e73
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-05-17 08:55:26 +0200

    tabs and shit

--- tri-sans-potentio.ino
+++ tri-sans-potentio.ino
@@ -3,40 +3,40 @@
 
    http://poubelle.online
 
-*/
+ */
 
 void tri() {
-	/* dont do anything if the weight is under 0 grams */
-	while (test_effort() <= 0)
-		message();
+  /* dont do anything if the weight is under 0 grams */
+  while (test_effort() <= 0)
+    message();
 
-	/* if the object is a metal */
-	if (test_inductif() == true) {
-		message();
-		Serial.println("metal");
-		Serial.println();
-	}
+  /* if the object is a metal */
+  if (test_inductif() == true) {
+    message();
+    Serial.println("metal");
+    Serial.println();
+  }
 
-	/* if the object is under 125 grams, it's plastic */
-	else {
-		if (test_effort() <= 125) {
-			message();
-			Serial.println("plastique");
-			Serial.println();
-		}
+  /* if the object is under 125 grams, it's plastic */
+  else {
+    if (test_effort() <= 125) {
+      message();
+      Serial.println("plastique");
+      Serial.println();
+    }
 
-	/* if the object is above 125 grams, it's glass */
-		else {
-			message();
-			Serial.println("verre");
-			Serial.println();
-		}
-	}
+    /* if the object is above 125 grams, it's glass */
+    else {
+      message();
+      Serial.println("verre");
+      Serial.println();
+    }
+  }
 }
 
 /* print the current weight */
 void message() {
-	Serial.print("poids: ");
-	Serial.print(test_effort());
-	Serial.println(" grammes");
+  Serial.print("poids: ");
+  Serial.print(test_effort());
+  Serial.println(" grammes");
 }
