8dda47b3821c46d69fd73fe41e4ea04d749bd5fa
Yann Le Guehennec <ylgdu56@gmail.com>
2018-05-25 11:49:16 +0200

    ca fonctionne

--- servo.ino
+++ servo.ino
@@ -1,14 +1,22 @@
 #include <ESP32_Servo.h>
 Servo servo_poubelle;
 
-int servoPin = 26;
+int servoPin = 26, pos = 0;
 
 void setup() {
+  Serial.begin(9600);
   servo_poubelle.attach(servoPin);
+  servo_poubelle.write(pos); // put the servo at 0
 }
 
 void loop() {
-  servo_poubelle.write(90);
+  for (pos = 0; pos <= 200; pos += 1) { // +
+    servo_poubelle.write(pos);
+    delay(15);
+  }
   delay(2000);
-  servo_poubelle.write(0);
+  for (pos = 200; pos >= 0; pos -= 1) { // -
+    servo_poubelle.write(pos);
+    delay(15);
+  }
 }
